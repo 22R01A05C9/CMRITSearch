d1fbf4c62537794d74df4863ad19c2489f8088e2
Ageera Saiteja <ageerasaiteja@gmail.com>
2025-04-08 18:33:18 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+
+const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+const app = express()
+
+app.use(function (req, res, next) {
+    try {
+        decodeURIComponent(req.path)
+        next()
+    } catch (err) {
+        //change after backend
+        res.redirect("https://saiteja.site")
+    }
+})
+app.use(cors())
+app.use(express.json({ limit: "30mb" }))
+
+require("./cmr.js")(app)
+
+server.listen(process.env.PORT, () => {
+    console.log("server staretd on http://localhost:" + process.env.PORT);
+})
